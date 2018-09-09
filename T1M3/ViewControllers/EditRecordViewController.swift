6087a512bf370585f20b55a70db3d0a26fcae7c5
Tam Lam <forestspirit1011@gmail.com>
2018-09-09 15:28:00 +1000

    add date and time input picker for date and time field

--- T1M3/ViewControllers/EditRecordViewController.swift
+++ T1M3/ViewControllers/EditRecordViewController.swift
@@ -18,6 +18,52 @@ class EditRecordViewController: UIViewController {
         super.viewDidLoad()
         let record = RecordLog.shared.getSelectedRecord()
         loadRecord(record: record)
+        setupInputPickers()
+        setupEndEditingOnTap()
+    }
+    
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    //Setup date and time keyboard inputs for date and time field
+    func setupInputPickers(){
+        var datePicker = UIDatePicker()
+        var timePicker = UIDatePicker()
+        timePicker.datePickerMode = .time
+        datePicker.datePickerMode = .date
+        dateTextField.inputView = datePicker
+        timeTextField.inputView = timePicker
+        datePicker.addTarget(self, action: #selector(EditRecordViewController.dateChanged(datePicker:)), for: .valueChanged)
+        timePicker.addTarget(self, action: #selector(EditRecordViewController.timeChanged(timePicker:)), for: .valueChanged)
+        
+    }
+    
+    //close keyboard on tap
+    func setupEndEditingOnTap(){
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(EditRecordViewController.viewTapped(gestureRecognizer:)))
+        view.addGestureRecognizer(tapGesture)
+    }
+    
+    //get date and time value from pickers
+    @objc func dateChanged(datePicker: UIDatePicker){
+        let dateFormater = DateFormatter()
+        dateFormater.dateFormat = "MM-dd-yyyy"
+        self.dateTextField.text = dateFormater.string(from: datePicker.date)
+        view.endEditing(true)
+    }
+    @objc func timeChanged(timePicker: UIDatePicker){
+        let timeFormater = DateFormatter()
+        timeFormater.dateFormat = "HH:mm"
+        self.timeTextField.text = timeFormater.string(from: timePicker.date)
+        view.endEditing(true)
+    }
+    
+    //quit editing mode on tap
+    @objc func viewTapped(gestureRecognizer: UITapGestureRecognizer){
+        view.endEditing(true)
     }
     func loadRecord(record: Recording){
         self.dateTextField.text = record.timeStarted.formatTimestamp(withFormat: "MM-dd-yyyy")
@@ -25,11 +71,6 @@ class EditRecordViewController: UIViewController {
         self.durationTextField.text = Recording.toHumanReadable(elapsedTime: record.finalRecordingElapsed)
         self.noteTextField.text = record.getNotes()
     }
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
 
     /*
     // MARK: - Navigation
